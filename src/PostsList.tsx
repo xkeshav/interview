492f1a1f192239a1aa208d8d97ec24643f148347
Keshav Mohta <xkeshav@gmail.com>
2024-07-19 00:49:30 +0530

    added working code

--- src/PostsList.tsx
+++ src/PostsList.tsx
@@ -8,8 +8,6 @@ type PostCardProps = {
 }
 
 function PostCard({ title, text, author }: PostCardProps) {
- 
-  
   return (
     <article className="post" >
       <h2>{title}</h2>
@@ -23,36 +21,41 @@ function PostCard({ title, text, author }: PostCardProps) {
 
 export default function PostLists() {
   const [data, setData] = useState<Post[]>([] as Post[]);
+  const scroller = useRef<HTMLDivElement|null>(null);
+  let isScrolled = false;
   const [pageNum, setPageNum] = useState(1);
-  const ref = useRef<any>();
-  
 
-  useEffect(()=>{
-    console.log(ref && ref.current);
-  }, [ref]);
 
   const fetchCurrentPost = async (num: number) => {
     const result = await fetchPosts(num);
+    console.log({result});
     setData(data.concat(result));
+    isScrolled = false;
   };
 
   const handleScroll = (e: any) => {
     const { clientHeight, offsetHeight, scrollHeight, scrollTop} = e.target;
-    //console.log(ref?.current)
     console.log({clientHeight, offsetHeight, scrollHeight, scrollTop});
-    //console.log(scrollHeight - (scrollTop + offsetHeight));
-    if(scrollHeight - (scrollTop + offsetHeight)  < 40) {
+    const totalHeight = offsetHeight + scrollTop ;
+    const heightDiff = Math.abs(scrollHeight - totalHeight);
+    console.log({heightDiff})
+    if(heightDiff < 50) {
       console.log('do fetch');
-      setPageNum(pageNum+1);
+      // FIX: still calling fetch call 2 3 times
+      !isScrolled && setPageNum(pageNum+1);
+      isScrolled = true;
     }
+    
   }
 
   useEffect(()=>{
+    scroller?.current?.addEventListener("scroll", handleScroll);
     fetchCurrentPost(pageNum);
+    return () =>  scroller?.current?.removeEventListener("scroll", handleScroll);
   }, [pageNum])
 
   return (
-    <div className="post-list" ref={ref} onScroll={handleScroll}> 
+    <div className="post-list" ref={scroller} > 
     {data.map((d, i)=> (
       <PostCard key={i} title={d.title} text={d.body} author={d.userId}/>
     ))}
