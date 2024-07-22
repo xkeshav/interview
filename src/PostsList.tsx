7f510fca9e18b5cc0085d5df7680c41e2d841894
Keshav Mohta <xkeshav@gmail.com>
2024-07-22 14:53:12 +0530

    added throttle but still having issues

--- src/PostsList.tsx
+++ src/PostsList.tsx
@@ -19,54 +19,62 @@ function PostCard({ title, text, author }: PostCardProps) {
   );
 }
 
+let isCalled = false;
+const throttle = (fn: (a: number) => any, delay = 1000) => {
+  if (isCalled) return;
+  isCalled = true;
+  return (...args: any) => {
+    fn(args);
+    setTimeout(()=>{
+      isCalled = false;
+    }, delay);
+  }
+}
+
 export default function PostLists() {
   const [data, setData] = useState<Post[]>([] as Post[]);
   const scroller = useRef<HTMLDivElement|null>(null);
   const [isScrolled, setIsScrolled]  = useState(false);
   const [pageNum, setPageNum] = useState(1);
-  const fetchRequestRef = useRef(true);
-  const pageNumRef = useRef(1);
 
 
   const fetchCurrentPost = async (num: number) => {
+    try {
     const result = await fetchPosts(num);
     console.log({result});
     setData(data.concat(result));
-    fetchRequestRef.current = true;
+    } catch(e: unknown) {
+      console.log('error while fetching ==>', (e as Error).message)
+    }
+    finally {
+      console.log('inside finally', isScrolled);
+      setIsScrolled(false);
+    }
   };
+
+
 
   const handleScroll = (e: any) => {
     const { clientHeight, offsetHeight, scrollHeight, scrollTop} = e.target;
-    console.log({clientHeight, offsetHeight, scrollHeight, scrollTop});
     const totalHeight = offsetHeight + scrollTop ;
     const heightDiff = Math.abs(scrollHeight - totalHeight);
-    console.log({heightDiff});
     document.body.style.setProperty('--scroll', heightDiff.toString() );
-    if(!isScrolled && heightDiff < 50) {
-      console.log('do fetch');
-      // FIX: still calling fetch call 2 3 times
-      if(fetchRequestRef.current) fetchRequestRef.current = false;
+    if(heightDiff <= 50) {
+      console.log({clientHeight, offsetHeight, scrollHeight, scrollTop});
+      setIsScrolled(true);
       setPageNum(pageNum+1);
-      //setIsScrolled(true);
     }
-    setIsScrolled(false);
-    // Adding timeout doesn't work
-    //setTimeout(() => {
-    //  setIsScrolled(false);
-    //}, 1);
-  }
+  };
 
   useEffect(()=>{
     scroller?.current?.addEventListener("scroll", handleScroll, false);
-    fetchCurrentPost(pageNum);
+    throttle(fetchCurrentPost(pageNum) as any, 10_000);
     return () =>  scroller?.current?.removeEventListener("scroll", handleScroll);
   }, [pageNum])
 
   return (
     <div className="post-list" ref={scroller} > 
-    {data.map((d, i)=> (
-      <PostCard key={i} title={d.title} text={d.body} author={d.userId}/>
-    ))}
+    {data.map((d, i)=> <PostCard key={i} title={d.title} text={d.body} author={d.userId}/>)}
     </div>
   );
 }
